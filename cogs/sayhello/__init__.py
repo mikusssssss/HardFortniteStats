5ceccd98091ee539b8a50cd593e9b0029ff84294
mikus <153551970+mikusssssss@users.noreply.github.com>
2026-03-04 23:56:54 +0100

    remove old over new repo

--- cogs/sayhello/__init__.py
+++
@@ -1,4 +0,0 @@
-from .sayhello import SayHello
-
-async def setup(bot):
-    await bot.add_cog(SayHello(bot))
--- cogs/serverstatus/__init__.py
+++
@@ -1,4 +0,0 @@
-from .serverstatus import serverstatus
-
-async def setup(bot):
-    await bot.add_cog(serverstatus(bot))
