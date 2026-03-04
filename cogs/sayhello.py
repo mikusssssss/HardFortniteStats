9d3bad79c44dd2fa3c4ee01343fa0f135e3b6fb8
mikus <153551970+mikusssssss@users.noreply.github.com>
2026-03-04 20:46:10 +0100

    ok

--- cogs/sayhello.py
+++ cogs/sayhello.py
@@ -8,3 +8,6 @@ class SayHello(commands.Cog):
     @discord.app_commands.command(name="say", description="repeats the message you sent")
     async def say(self, interaction: discord.Interaction, message: str):
         await interaction.response.send_message(f"{interaction.user.display_name}: {message}")
+    @discord.app_commands.command(name="saywithimage", description="repeats message with added image")
+    async def saywithimage(self, interaction: discord.Interaction, message: str, image: discord.Attachment):
+        await interaction.response.send_message(f"{interaction.user.display_name}: {message}\n{image.url}")
