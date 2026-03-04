d7e3462b2c63bb3b245bd9256f459622dfa97ef8
mikus <153551970+mikusssssss@users.noreply.github.com>
2026-03-04 22:19:34 +0100

    clarifies

--- cogs/sayhello/sayhello.py
+++ cogs/sayhello/sayhello.py
@@ -9,7 +9,7 @@ class SayHello(commands.Cog):
     async def say(self, interaction: discord.Interaction, message: str):
         await interaction.response.send_message(f"{interaction.user.display_name}: {message}")
 
-    @discord.app_commands.command(name="saywithimage", description="repeats the message you sent with an added image")
+    @discord.app_commands.command(name="saywithimage", description="repeats the message you sent with an added image (WONT WORK IF YOU ARE BANNED)")
     async def saywithimage(self, interaction: discord.Interaction, message: str, image: discord.Attachment):
         await interaction.response.send_message(f"{interaction.user.display_name}: {message}\n{image.url}")
 
